0b991a46f83c536319cdbb8649d475fe1dd62507
Nikita <latinisnotdead07@gmail.com>
2020-06-24 16:23:30 +0100

    comments out code in controller to start TDD

--- note-controller.js
+++ note-controller.js
@@ -1,23 +1,23 @@
-(function (exports) {
-  function NoteController(NoteList) {
-    this.notelist = NoteList;
-    this.notelist.addNote('Beer');
-    this.view = new NoteListView(this.notelist);
-  }
+// (function (exports) {
+//   function NoteController(NoteList) {
+//     this.notelist = NoteList;
+//     this.notelist.addNote('Beer');
+//     this.view = new NoteListView(this.notelist);
+//   }
 
-  NoteController.prototype.renderHtml = function () {
-    var test = document.getElementById('app');
-    test.innerHTML = this.view.htmlWrap();
-  };
+//   NoteController.prototype.renderHtml = function () {
+//     var test = document.getElementById('app');
+//     test.innerHTML = this.view.htmlWrap();
+//   };
 
-  exports.NoteController = NoteController;
+//   exports.NoteController = NoteController;
 
-})(this);
+// })(this);
 
-document.addEventListener('DOMContentLoaded', function (event) {
-  var list = new NoteList
-  var controller = new NoteController(list)
-  controller.renderHtml();
-  });
+// document.addEventListener('DOMContentLoaded', function (event) {
+//   var list = new NoteList
+//   var controller = new NoteController(list)
+//   controller.renderHtml();
+//   });
 
 
