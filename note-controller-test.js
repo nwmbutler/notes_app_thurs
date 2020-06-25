7d1075fc44b45ae2416a5aea82343edcd5e3bd8a
Nick Butler <nwmbutler@gmail.com>
2020-06-25 11:50:57 +0100

    note-controller-test passing and fully functioning

--- note-controller-test.js
+++ note-controller-test.js
@@ -1,10 +1,21 @@
+function docReady(fn) {
+ 
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}    
+
+docReady(function(){
+
 function instantiateNoteController() {
   var list = new NoteList;
   var controller = new NoteController(list);
   assert.isTrue(controller.noteListModel instanceof NoteList);
 }
 
-function innnerHTMLContainsPassedFavourite() {
+function innerHTMLContainsPassedFavourite() {
   var list = new NoteList;
   list.addNote("Pesto")
   var controller = new NoteController(list);
@@ -14,4 +25,6 @@ function innnerHTMLContainsPassedFavourite() {
 }
 
 instantiateNoteController();
-innnerHTMLContainsPassedFavourite();
+innerHTMLContainsPassedFavourite();
+
+})
--- note-controller.js
+++ note-controller.js
@@ -5,36 +5,9 @@
   };
 
   NoteController.prototype.renderHtml = function() {
-    var test = document.getElementById('app');
+    test = document.getElementById("app");
     test.innerHTML = this.view.htmlWrap();
   }
   exports.NoteController = NoteController;
 })(this);
-    
-    
-
-
-
-// (function (exports) {
-//   function NoteController(NoteList) {
-//     this.notelist = NoteList;
-//     this.notelist.addNote('Beer');
-//     this.view = new NoteListView(this.notelist);
-//   }
-
-//   NoteController.prototype.renderHtml = function () {
-//     var test = document.getElementById('app');
-//     test.innerHTML = this.view.htmlWrap();
-//   };
-
-//   exports.NoteController = NoteController;
-
-// })(this);
-
-// document.addEventListener('DOMContentLoaded', function (event) {
-//   var list = new NoteList
-//   var controller = new NoteController(list)
-//   controller.renderHtml();
-//   });
-
-
+  
